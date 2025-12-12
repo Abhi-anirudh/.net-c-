e3a3f97af2d2d8dea189c0c37cdb0cf91a88ba2f
ANIRUDH K <147629094+Abhi-anirudh@users.noreply.github.com>
2025-12-12 06:27:05 +0000

    new

--- StudentApp/obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
+++ StudentApp/obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6498af4a57b6e13d87a4ea4240cde938dee86e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e970fd055d4b54bc7cfa5e23b1be89f7b3cdf32")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
